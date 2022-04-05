080242b386b6726272afc14984748dd08e4c84e9
sakamoto-waka <wakanmhl@gmail.com>
2022-04-05 02:27:55 +0000

    [fix] routing + header user_signed_in?→customer_signed_in?

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
-  root => 'homes#top'
+  root to: 'homes#top'
   get 'about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
