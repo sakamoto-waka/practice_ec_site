5f7ca4f80e1bdb0e430e642f9acac8ba9da7e12a
sakamoto-waka <wakanmhl@gmail.com>
2022-04-05 01:56:31 +0000

    [update] public/sessions_controller customer_state method

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -18,7 +19,18 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  # 退会しているユーザーか判断
+  def customer_state
+    @customer = Customer.fund_by(email: params[:customer][:email])
+    # アカウントを取得できなければ（ユーザーがいなければ）そのままreturn
+    return unless @customer
+    if @customer.valid_password?(params[:customer][:password]) && !@customer.is_deleted　
+        redirect_to public_signup_path
+
+    end
+  end
+  
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
